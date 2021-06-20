11b94c69a93ee616a1c0e5b8fd1f089202d6498b
mehrajratul <mehrajratul@gmail.com>
2021-06-21 00:54:12 +0600

    deploy

---
+++ assignment.js
@@ -0,0 +1,58 @@
+// kilometerToMeter
+                                                            // 1 km = 1000m
+function kilometerToMeter(kiloMeter) {
+    var meter = 1000 * kiloMeter;               
+    return meter;
+}
+
+// budgetCalculator
+                                                            // watch = 50
+                                                            // phone= 100
+                                                            // laptop = 500
+function budgetCalculator(watch, phone, laptop) {
+    var watchCount = watch * 50;
+    var phoneCount = phone * 100;
+    var laptopCount = laptop * 500;
+    var totalSum = watchCount + phoneCount + laptopCount;
+    return totalSum;   
+}
+
+// hotelCost
+
+
+function hotelCost(totalDays) {
+    var cost = 0;
+    if (totalDays <= 10){
+        cost = totalDays * 100;
+    }
+    else if (totalDays <= 20){
+        var previousCost = 10 * 100;
+        var remaining = totalDays - 10;
+        var secondHalfCost = remaining * 80;
+        cost = previousCost + secondHalfCost;
+    }
+    else {
+        var previousCost = 10 * 100;
+        var secondHalfCost = 10 * 80;
+        var remaining = totalDays - 20;
+        var remainingDaysCost = remaining * 50;
+        cost = previousCost + secondHalfCost + remainingDaysCost;
+    }
+    return cost;
+}
+
+
+//megaFriend
+
+
+
+function megaFriend(names) {
+    var megaName = names[0];
+    for(var i = 0; i < names.length; i++){
+        var friend = names[i];
+        if (friend.length > megaName.length) {
+            megaName = friend;
+        }
+    }
+    return megaName;
+}
